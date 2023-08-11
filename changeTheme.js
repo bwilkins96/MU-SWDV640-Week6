1acbf4094d096b5849f433d450a650d8046f9449
bwilkins96 <ben.jr.speed@gmail.com>
2023-08-11 16:32:14 -0400

    dark / light theme

--- changeTheme.js
+++ changeTheme.js
@@ -1,16 +1,27 @@
 // Change theme of page between dark and light
 
+function switchIcon(icon, toNightTheme = true) {
+    if (toNightTheme) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }   
+}
+
 function changeTheme() {
     const body = document.body;
     const icon = document.getElementById('themeSwitcher');
+    const altIcon = document.getElementById('altThemeSwitcherIcon');
 
     if (body.classList.contains('darkTheme')) {
         body.classList.remove('darkTheme');
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
+        switchIcon(icon, false);
+        switchIcon(altIcon, false);       
     } else {
         body.classList.add('darkTheme');
-        icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon');
+        switchIcon(icon);
+        switchIcon(altIcon);   
     }
 }    
